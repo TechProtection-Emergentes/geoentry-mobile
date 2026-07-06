3fc772e6e9fe6bba750eebf6e7de4ba07605e705
RodrLH <u202212338@upc.edu.pe>
2026-07-06 09:27:04 -0500

    feat: initialize Supabase client and add generated database schema types

--- src/supabase/supabase-client.ts
+++ src/supabase/supabase-client.ts
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto'
 import { createClient } from '@supabase/supabase-js'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const supabaseUrl = 'https://njrccrvfsnmvnbwvlvey.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qcmNjcnZmc25tdm5id3ZsdmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxNTQ4NjEsImV4cCI6MjA2MjczMDg2MX0.BsxMyKpmDqcxgMVkJeLNT0JAKj5epWTJnUOZx6auPmU'
+const supabaseUrl = 'https://swdncevmrdcjgrgtkjkv.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3ZG5jZXZtcmRjamdyZ3Rramt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwMDA1MTMsImV4cCI6MjA5ODU3NjUxM30.0ELCmNd-ln5sPP403bROCGOk79wfiO0pnX-hu-mtha4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
